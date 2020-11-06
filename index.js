c3c6ef87e68b05fc5fb2b8dd3bbd3ce16b36d4f4
drmikecrowe <drmikecrowe@gmail.com>
2020-11-06 04:45:04 -0500

    Adding support for pnpm

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-module.exports = require('../../.eslintplugin')
+const { existsSync } = require('fs');
+const { dirname } = require('path');
+
+let dir = dirname(__dirname);
+let rfile = `${dir}/.eslintplugin`;
+while (dir !== "//" && !existsSync(`${rfile}.js`)) {
+    dir = dirname(dir);
+    rfile = `${dir}/.eslintplugin`;
+}
+module.exports = require(rfile);
